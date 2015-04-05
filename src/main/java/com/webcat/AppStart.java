c5ba68397e9517c67f0cb748bf053e6997a0f63a
bkawli <bhavik.kawli@gmail.com>
2015-04-05 20:22:24 +0530

    xxxxxxxxxx

--- src/main/java/com/webcat/AppStart.java
+++ src/main/java/com/webcat/AppStart.java
@@ -1,6 +1,9 @@
 package com.webcat;
 import static spark.Spark.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
@@ -10,12 +13,12 @@ import spark.*;
 
 public class AppStart {
 
-	public static void main(String[] args) throws Exception {
-		
+	public static void main(String[] args) throws Exception {		
 		
 		String IP_ADDRESS = System.getenv("OPENSHIFT_DIY_IP");		
 		int PORT;
 		String REPO_DIR;
+		String DATA_DIR;
 		
 		DBConnector dbconn=new DBConnector();
 		final DB db=dbconn.getDB();
@@ -24,35 +27,72 @@ public class AppStart {
 		{
 			IP_ADDRESS = "localhost";		
 			PORT = 8080;
-			REPO_DIR=System.getProperty("user.dir");			
+			REPO_DIR=System.getProperty("user.dir") + "/resources/public/";	
+			DATA_DIR="E:/CatalogApp_Data_dir/";
 		}
 		else
 		{	
 			PORT = Integer.parseInt(System.getenv("OPENSHIFT_DIY_PORT"));	
-			REPO_DIR=System.getenv("OPENSHIFT_REPO_DIR");			
+			REPO_DIR=System.getenv("OPENSHIFT_REPO_DIR") + "resources/public/";	
+			DATA_DIR=System.getenv("OPENSHIFT_DATA_DIR");
 		}
-		externalStaticFileLocation(REPO_DIR + "/resources/public/");
+		externalStaticFileLocation(DATA_DIR + "public/");
 		setIpAddress(IP_ADDRESS);
-		setPort(PORT);
-		
+		setPort(PORT);		
 		
 		
 		get("/",new Route(){
-			 public Object handle(Request request, Response response) {						 
-				 response.redirect("index.html");
-				 return "";	             
-	            }
+			public Object handle(Request request, Response response) 
+			{					 
+				response.redirect("home");			 
+				return "";	             
+			}
+		});
+		
+		get("/home",new Route(){
+			public Object handle(Request request, Response response) 
+			{		
+				String html = "";
+				try 
+				{
+					html=getStringFromFile(REPO_DIR + "home.html");
+				} 
+				catch (Exception e) 
+				{					
+					e.printStackTrace();
+				}
+				return html;	             
+			}
 		});
 		
 		get("/json",new Route(){
-			 public Object handle(Request request, Response response) {						 
-				 DBCollection coll = db.getCollection("testcoll");
-				 DBObject myDoc = coll.findOne();				 
-				 String dbstr=myDoc.toString();					 		 
-				 return dbstr;	             
-	            }
+			public Object handle(Request request, Response response) 
+			{						 
+				DBCollection coll = db.getCollection("testcoll");
+				DBObject myDoc = coll.findOne();				 
+				String dbstr=myDoc.toString();					 		 
+				return dbstr;	             
+			}
 		});
 
+	}
+	
+	
+	public static String getStringFromFile(String file_path) throws Exception
+	{
+		File fl=new File(file_path);
+		FileInputStream is=new FileInputStream(fl);
+		byte[] container_data=null;	
+		ByteArrayOutputStream container = new ByteArrayOutputStream();
+        byte[] buf = new byte[8*1024];
+        int read;
+        while ((read = is.read(buf, 0, 8*1024)) > 0) 
+        {
+            container.write(buf, 0, read);            
+        }			
+        container_data=container.toByteArray();   
+        is.close();
+        return new String(container_data);
 	}
 
 }
