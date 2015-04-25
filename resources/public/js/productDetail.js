c7ce76306216e2d946548b22f82645bb210efd15
bkawli <bhavik.kawli@gmail.com>
2015-04-25 16:24:04 +0530

    xxxxxxxxxxxxxxxxxxxx

--- resources/public/js/productDetail.js
+++ resources/public/js/productDetail.js
@@ -4,7 +4,7 @@ app.controller('ProductCtrl', function($scope,$location,$http) {
 	var url=$location.absUrl();
 	var baseurl='/products';
 	var indx=url.indexOf(baseurl) + baseurl.length + 1;
-	$scope.name=url.substr(indx);
+	var name=url.substr(indx);
 	var resp=$http.get("/getProductDetails/" + name);	
 	resp.success(function(data) {$scope.product = data;});	
 });
