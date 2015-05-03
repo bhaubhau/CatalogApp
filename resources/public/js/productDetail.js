7d49550a439c0c92bd9675650d4c97dbe6a95dcd
bkawli <bhavik.kawli@gmail.com>
2015-05-03 20:21:58 +0530

    xxxxxxxxxxxxxxxxxxxxxx

--- resources/public/js/productDetail.js
+++ resources/public/js/productDetail.js
@@ -1,10 +1,14 @@
 var app=angular.module('product',[]);
 
-app.controller('ProductCtrl', function($scope,$location,$http) {
+app.controller('ProductCtrl',['$scope','$location','$http',function($scope,$location,$http) {
 	var url=$location.absUrl();
 	var baseurl='/products';
 	var indx=url.indexOf(baseurl) + baseurl.length + 1;
 	var name=url.substr(indx);
 	var resp=$http.get("/getProductDetails/" + name);	
-	resp.success(function(data) {$scope.product = data;});	
-});
+	resp.success(function(data) {$scope.product = JSON.parse(data);});		
+	$scope.minindex=1;
+	//$scope.maxindex=$scope.product.Images.length;
+	
+}]);
+
--- resources/public/js/products.js
+++ resources/public/js/products.js
@@ -1,7 +1,7 @@
 var app=angular.module('productsApp',[]);
 
-app.controller('MainCtrl', function($scope, $http) {
+app.controller('MainCtrl', ['$scope','$http',function($scope, $http) {
 	$scope.products={};
 	var resp=$http.get("/getProducts");	
 	resp.success(function(data) {$scope.products = data.products;});	
-});
+}]);
