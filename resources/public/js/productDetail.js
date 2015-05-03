d7172de2e0ce61615a8b654a51ca1f0feecc30dc
bkawli <bhavik.kawli@gmail.com>
2015-05-03 21:32:21 +0530

    xxxxxxxxxxxxxxxxxxxxxxxx

--- resources/public/js/productDetail.js
+++ resources/public/js/productDetail.js
@@ -8,8 +8,15 @@ app.controller('ProductCtrl',['$scope','$location','$http',function($scope,$loca
 	var resp=$http.get("/getProductDetails/" + name);	
 	resp.success(function(data) {$scope.product = data;});		
 	$scope.minindex=1;
+	//sample product
+	//{ "ProductName" : "Chains" , "Index" : 16 , "Images" : [ "IMG_07.jpg" , "IMG_08.jpg" , "IMG_05.jpg" , "IMG_04.jpg" , "IMG_03.jpg" , "IMG_06.jpg" , "IMG_02.jpg" , "IMG_01.jpg"]}
+	
+	//var obj=JSON.parse($scope.product);
 	//$scope.maxindex=$scope.product.Images.length;	
-	$scope.maxindex=Object.keys('$scope.product.Images').length;
+	//$scope.maxindex=Object.keys('$scope.product.Images').length;
+	//$scope.maxindex=obj.Images.length;
+	//var obj=eval("(" + $scope.product + ")");
+	//$scope.maxindex=Object.keys(obj).length;
 	
 }]);
 
