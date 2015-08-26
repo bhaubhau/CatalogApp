529d3eee63544472e1ad382c2cffbe45fb8c72c6
bkawli <bhavik.kawli@gmail.com>
2015-08-26 23:34:33 +0530

    bnnnnnnn

--- resources/public/js/upload.js
+++ resources/public/js/upload.js
@@ -1,12 +1,33 @@
-var doc = document.documentElement;
-doc.ondragover = function () { this.className = 'hover'; return false; };
-doc.ondragend = function () { this.className = ''; return false; };
-doc.ondrop = function (event) {
-  event.preventDefault && event.preventDefault();
-  this.className = '';
+var app=angular.module('uploadApp',[]);
 
-  // now do something with:
-  var files = event.dataTransfer.files;
+app.controller('UploadCtrl', ['$scope','$http',function($scope,$http) {
+	$scope.attachedFile = function(element) {
+		$scope.$apply(function($scope) {
+			$scope.file = element.files[0];   
+		});		
+		console.log('file attached');
+	};
+	
+	$scope.addFile = function() {		
+		var fd = new FormData();		
+		fd.append("file",$scope.file);			
+		$http(
+				{method:'POST',
+				url: '/upload', 
+				headers: {
+					   'Content-Type': undefined
+					 },
+				data: fd}
+				
+		)
+		.success(function(data, status, headers, config) {
+			console.log('success');
+		})
+		.error(function(data, status, headers, config) {
+			console.log('error');
+		})
+	};
+}])
 
-  return false;
-};
+
+
